ade437c442d18558f2c965e60db5092e321a1796
Ivan <krew701@gmail.com>
2022-08-03 11:43:40 +0300

    second task console app

--- script.js
+++ script.js
@@ -21,10 +21,23 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+// Lesson 2 
+// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+// возвращаем пользователя к вопросам опять
+
+// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+// 4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
-console.log(numberOfFilms);
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -33,16 +46,48 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-console.log(personalMovieDB.count);
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log('"Произошла ошибка"');
+}
+
 
 for (let i = 0; i < 2; i++) {
     const lastViewedFilm = prompt('Один из последних просмотренных фильмов?', '');
     const lastViewedFilmRate = +prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[lastViewedFilm] = lastViewedFilmRate;
+    if (lastViewedFilm != null && lastViewedFilmRate != null && lastViewedFilm != '' && lastViewedFilmRate != '' && lastViewedFilm.length < 50) {
+        personalMovieDB.movies[lastViewedFilm] = lastViewedFilmRate;
+        console.log('well done');
+    } else {
+        console.log('some error');
+        i--;
+    }
 }
 
-console.log(personalMovieDB.movies);
+// let i = 0;
+// while ( i < 2 ) {
+//     const lastViewedFilm = prompt('Один из последних просмотренных фильмов?', '');
+//     const lastViewedFilmRate = +prompt('На сколько оцените его?', '');
+
+//     if (lastViewedFilm != null && lastViewedFilmRate != null && lastViewedFilm != '' && lastViewedFilmRate != '' && lastViewedFilm.length < 50) {
+//         personalMovieDB.movies[lastViewedFilm] = lastViewedFilmRate;
+//         console.log('well done');
+//         i++;
+//     } else {
+//         console.log('some error');
+//         i--;
+//     }
+// }
+
+
+// console.log(personalMovieDB.movies);
 console.log(personalMovieDB);
 
 
